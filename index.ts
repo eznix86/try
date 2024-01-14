bed23220a2e028bdb546df27d50ab0a9fbb37a7e
Bruno Bernard <contact.brunobernard@gmail.com>
2024-01-15 00:53:21 +0400

    🐛 fix: refactor exports in index.ts

--- index.ts
+++ index.ts
@@ -2,4 +2,4 @@ import Result from "~/result";
 import { tryAsync } from "~/try-async";
 import { trySync } from "~/try-sync";
 
-export default { Result, tryAsync, trySync };
+export { Result, tryAsync, trySync };
