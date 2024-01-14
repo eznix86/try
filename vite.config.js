90038b2f0516367ec130ee41f55083ed67ebcc25
Bruno Bernard <contact.brunobernard@gmail.com>
2024-01-15 00:52:31 +0400

    ✅ chore: update package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,30 @@
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-import { fileURLToPath } from "node:url";
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: fileURLToPath(new URL("index.ts", import.meta.url)),
-      name: "try-async",
-      fileName: "try-async",
-    },
-  },
-  plugins: [dts()],
-  resolve: {
-    alias: [
-      {
-        find: "~",
-        replacement: fileURLToPath(new URL("./lib", import.meta.url)),
-      },
-    ],
-  },
+    mode: "production",
+	build: {
+        minify: "esbuild",
+		lib: {
+			entry: fileURLToPath(new URL("index.ts", import.meta.url)),
+			name: "@eznix/try",
+			fileName: "try",
+		},
+	},
+	plugins: [
+		dts({
+            insertTypesEntry: true,
+			pathsToAliases: true,
+			rollupTypes: true,
+		}),
+	],
+	resolve: {
+		alias: [
+			{
+				find: "~",
+				replacement: fileURLToPath(new URL("./lib", import.meta.url)),
+			},
+		],
+	},
 });
